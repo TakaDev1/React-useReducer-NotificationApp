45cf2fa109db6152fb81793835e05eafc7611bbc
Izumi Takayuki <r8quattrospider@gmail.com>
2026-09-10 20:32:59 +0900

    feat: Implement NotificationList Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import ControlPanel from "./feature/components/ControlPanel";
+import NotificationList from "./feature/components/NotificationList";
 import { NotificationProvider } from "./feature/contexts/NotificationContext";
 
 function App() {
   return (
     <div>
       <NotificationProvider>
-        <div></div>
+        <div>
+          <NotificationList />
+          <ControlPanel />
+        </div>
       </NotificationProvider>
     </div>
   );
